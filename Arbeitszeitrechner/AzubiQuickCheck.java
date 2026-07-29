21916998c357b1c401b2c5cf02b483eae468c7d1
7coffeeandcode <zou23@tutamail.com>
2026-07-29 11:30:36 +0200

    det

--- Arbeitszeitrechner/AzubiQuickCheck.java
+++ Arbeitszeitrechner/AzubiQuickCheck.java
@@ -1,7 +1,7 @@
 import java.util.Locale; // Landessprache, hier Komma statt Punkt
 import java.util.Scanner; // Benutzereingaben einlesen über Tastatur (System.in)
 import java.time.LocalTime; // speichert und verarbeitet reine Uhrzeiten
-import java.time.format.DateTimeFormatter; //formattiert Daten in Zeitobjekte mit definiertem 
+import java.time.format.DateTimeFormatter; //formattiert Daten in Zeitobjekte mit definiertem Format
 import java.time.format.DateTimeParseException; //fängt fehlerhafte Zeiteingaben ab
 
 public class AzubiQuickCheck{
